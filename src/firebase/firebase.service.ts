a5d730a14e541a951f3dc728647f5ddf0dc026ef
Jardson Alan <jardsonalan132@gmail.com>
2026-05-23 00:53:58 -0300

    feat: Adiciona tratamento de erros para autenticação e requisições REST no serviço Firebase

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import * as firebaseAdmin from 'firebase-admin';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRequest, DecodedIdToken, UserRecord } from 'firebase-admin/auth';
 import { FirebaseConfigService } from './firebase-config.service';
 import axios from 'axios';
@@ -14,11 +19,17 @@ export class FirebaseService {
   }
 
   async createUser(props: CreateRequest): Promise<UserRecord> {
-    return await firebaseAdmin.auth().createUser(props);
+    return (await firebaseAdmin
+      .auth()
+      .createUser(props)
+      .catch(this.handleFirebaseAuthError)) as UserRecord;
   }
 
   async verifyIdToken(token: string): Promise<DecodedIdToken> {
-    return await firebaseAdmin.auth().verifyIdToken(token);
+    return (await firebaseAdmin
+      .auth()
+      .verifyIdToken(token)
+      .catch(this.handleFirebaseAuthError)) as DecodedIdToken;
   }
 
   async signInWithEmailAndPassword(email: string, password: string) {
@@ -27,7 +38,7 @@ export class FirebaseService {
       email,
       password,
       returnSecureToken: true,
-    });
+    }).catch(this.handleRestApiError);
   }
 
   private async sendPostRequest(url: string, data: any) {
@@ -35,5 +46,27 @@ export class FirebaseService {
       headers: { 'Content-Type': 'application/json' },
     });
     return response.data;
+  }
+
+  private handleFirebaseAuthError(error: any) {
+    if (error.code?.startsWith('auth/')) {
+      throw new BadRequestException(error.message);
+    }
+    throw new Error(error.message);
+  }
+
+  private handleRestApiError(error: any) {
+    if (error.response?.data?.error?.code === 400) {
+      const messageKey = error.response?.data?.error?.message;
+      const message =
+        {
+          INVALID_LOGIN_CREDENTIALS: 'Invalid login credentials.',
+          INVALID_REFRESH_TOKEN: 'Invalid refresh token.',
+          TOKEN_EXPIRED: 'Token expired.',
+          USER_DISABLED: 'User account is disabled.',
+        }[messageKey] ?? messageKey;
+      throw new BadRequestException(message);
+    }
+    throw new Error(error.message);
   }
 }
