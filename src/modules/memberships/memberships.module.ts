675d082431b4764f86024a4f298b98d83bba2d05
Jardson Alan <jardsonalan132@gmail.com>
2026-05-27 18:38:44 -0300

    fix(memberships): Atualiza importações para simplificar referências de guardas e decoradores

--- src/modules/memberships/memberships.module.ts
+++ src/modules/memberships/memberships.module.ts
@@ -3,9 +3,10 @@ import { MembershipsService } from './memberships.service';
 import { MembershipsController } from './memberships.controller';
 import { PrismaModule } from '@/database/prisma/prisma.module';
 import { FirebaseModule } from '@/firebase/firebase.module';
+import { CommonModule } from '@/common/common.module';
 
 @Module({
-  imports: [PrismaModule, FirebaseModule],
+  imports: [PrismaModule, FirebaseModule, CommonModule],
   controllers: [MembershipsController],
   providers: [MembershipsService],
   exports: [MembershipsService],
