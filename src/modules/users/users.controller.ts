b7e0726392afded610c419e347300fe8c96cade3
Jardson Alan <jardsonalan132@gmail.com>
2026-05-26 21:52:23 -0300

    feat(users): Adiciona endpoint para obter associações de usuário e integra o módulo de associações

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { FirebaseService } from '@/firebase/firebase.service';
+import { MembershipsService } from '../memberships/memberships.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { IdToken } from '../auth/id-token.decorator';
 import { AuthGuard } from '../auth/auth.guard';
@@ -10,6 +17,7 @@ export class UsersController {
   constructor(
     private readonly usersService: UsersService,
     private readonly firebaseService: FirebaseService,
+    private readonly membershipsService: MembershipsService,
   ) {}
 
   @Get('profile')
@@ -24,5 +32,16 @@ export class UsersController {
       firebase: firebaseData,
       profile: dbData,
     };
+  }
+
+  @Get(':userId/memberships')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async getMemberships(
+    @IdToken() token: string,
+    @Param('userId', ParseIntPipe) userId: number,
+  ) {
+    const firebaseData = await this.firebaseService.verifyIdToken(token);
+    return this.membershipsService.findByUserId(firebaseData.uid, userId);
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MembershipsModule } from '../memberships/memberships.module';
 
 @Module({
+  imports: [MembershipsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
